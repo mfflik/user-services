98bccbcf3debc552141df6de3f5b9fc2cb7f18e4
mfflik <muh.fauzan.f@gmail.com>
2023-05-15 16:40:50 +0700

    1 : Membuat Add dan Remove User dengan Postgres

---
+++ src/services/userService.js
@@ -0,0 +1,26 @@
+import { find, create } from '../models/User';
+
+async function getUsers() {
+  try {
+    const users = await find();
+    return users;
+  } catch (error) {
+    console.error('Gagal mengambil daftar pengguna:', error);
+    throw error;
+  }
+}
+
+async function addUser(username, email, password, role) {
+  try {
+    const newUser = await create({ username, email, password, role });
+    return newUser;
+  } catch (error) {
+    console.error('Gagal menambahkan pengguna baru:', error);
+    throw error;
+  }
+}
+
+export default {
+  getUsers,
+  addUser
+};
